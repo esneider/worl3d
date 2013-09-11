b8474d7c872413f9cb0f1f3c3c6d6115bf2f7a83
esneider <dariosn@gmail.com>
2013-09-11 00:19:31 -0300

    Refactored code and made it jshint compliant.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,122 @@
-var camera, scene, renderer;
-var geometry, material, mesh;
+/* jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true,
+          strict:true, undef:true, unused:true, curly:true, browser:true,
+          devel:true, jquery:true, indent:4, maxerr:50 */
 
-function init() {
+/* global THREE, requestAnimationFrame */
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+(function() {
 
-    scene = new THREE.Scene();
+    "use strict";
 
-    geometry = new THREE.CubeGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    var world = {};
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    function newCamera() {
 
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+        var viewAngle = 75; // from bottom to top of view, in degrees
+        var aspectRatio = window.innerWidth / window.innerHeight;
+        var nearPlane = 1;
+        var farPlane = 10000;
 
-    document.body.appendChild( renderer.domElement );
-}
+        var camera = new THREE.PerspectiveCamera(viewAngle, aspectRatio, nearPlane, farPlane);
 
-function animate() {
+        camera.position.z = 1000;
 
-    // note: three.js includes requestAnimationFrame shim
-    requestAnimationFrame( animate );
+        return camera;
+    }
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    function newObjects() {
 
-    renderer.render( scene, camera );
-}
+        var objs = {};
 
-$(document).ready(function() {
+        objs.axes = new THREE.AxisHelper();
+        objs.axes.scale.set(100, 100, 100);
 
-    init();
-    animate();
-})
+        var geometry = new THREE.CubeGeometry(200, 200, 200);
+        var material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+        objs.cube = new THREE.Mesh(geometry, material);
+
+        return objs;
+    }
+
+    function newScene(objects) {
+
+        var scene = new THREE.Scene();
+
+        for (var obj in objects) {
+            if (objects.hasOwnProperty(obj)) {
+                scene.add(objects[obj]);
+            }
+        }
+
+        return scene;
+    }
+
+    function newRenderer() {
+
+        var renderer;
+
+        if (window.WebGLRenderingContext) {
+            renderer = new THREE.WebGLRenderer();
+        } else {
+            renderer = new THREE.CanvasRenderer();
+        }
+
+        renderer.setClearColor(new THREE.Color(0x000000));
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        return renderer;
+    }
+
+    function initWorld() {
+
+        world.viewPort = {
+            width:  window.innerWidth,
+            height: window.innerHeight
+        };
+
+        world.camera = newCamera();
+        world.objects = newObjects();
+        world.scene = newScene(world.objects);
+        world.renderer = newRenderer();
+
+        document.body.appendChild(world.renderer.domElement);
+    }
+
+    function animate() {
+
+        requestAnimationFrame(animate);
+
+        world.objects.cube.rotation.x += 0.01;
+        world.objects.cube.rotation.y += 0.02;
+
+        world.renderer.render(world.scene, world.camera);
+    }
+
+    $(document).ready(function() {
+
+        initWorld();
+        animate();
+    });
+
+    $(window).resize(function() {
+
+        // var widthRatio  = window.innerWidth  / $(this).data("info").width;
+        // var heightRatio = window.innerHeight / $(this).data("info").height;
+
+        // var ratio = Math.min(widthRatio, heightRatio);
+
+        // initCamera();
+
+        // camera.position.z *= ratio;
+
+        // camera.aspect = window.innerWidth / window.innerHeight;
+        // camera.updateProjectionMatrix();
+
+        // console.log(camera.position.z);
+
+        // renderer.setSize(window.innerWidth, window.innerHeight);
+        // renderer.render(scene, camera);
+    });
+
+})();
 
